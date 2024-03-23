daee04a35ca40b2cd76f129346f71f2af680be1a
Mohammad Rahad <126962816+MR-RAHAD-511@users.noreply.github.com>
2024-03-23 16:31:32 +0600

    Update convert2.js

--- scripts/commands/convert2.js
+++ scripts/commands/convert2.js
@@ -1,16 +1,15 @@
 const axios = require('axios');
 const fs = require('fs');
-const path = require('path');
 
 module.exports.config = {
     name: "dl",
     version: "1.0.0",
     permission: 2,
     credits: "Rahad",
-    description: "Convert media from a Google Drive link",
+    description: "Download media from a Google Drive link",
     prefix: true, 
     category: "Media", 
-    usages: "/Convert [Google Drive link]",
+    usages: "/dl [Google Drive link]",
     cooldowns: 5,
     dependencies: {
         "axios": "",
@@ -21,43 +20,49 @@ module.exports.config = {
 module.exports.run = async function ({ api, event, args }) {
     const url = args[0];
 
-    if (!url || !url.includes('drive.google.com')) {
+    if (!url) {
         return api.sendMessage('Please provide a valid Google Drive link.', event.threadID, event.messageID);
     }
   
     try {
-        // Extract the file ID from the Google Drive link
-        const fileId = url.match(/\/file\/d\/(.+?)\//)[1];
+        // Modify the Google Drive link to a direct download link
+        const directDownloadLink = modifyToDirectDownloadLink(url);
 
-        // Construct the new download link
-        const downloadLink = `https://drive.google.com/uc?export=download&id=${fileId}`;
-
-        const response = await axios.get(downloadLink, { responseType: 'arraybuffer' });
+        // Fetch the media content from the modified direct download link
+        const response = await axios.get(directDownloadLink, { responseType: 'arraybuffer' });
 
         if (response.status !== 200) {
             return api.sendMessage('Failed to fetch the media from the provided link.', event.threadID, event.messageID);
         }
 
-        // Extract the file extension from the URL
-        const extension = path.extname(url);
-
-        // Generate a random filename
-        const filename = `converted${extension}`;
-
+        // Generate a random filename with the same extension as the original file
+        const filename = `downloaded${getFileExtension(url)}`;
+        
         // Write the downloaded data to a file
         fs.writeFileSync(filename, Buffer.from(response.data, 'binary'));
 
-        // Send the file as an attachment
+        // Send the downloaded file as an attachment
         api.sendMessage(
             {
-                body: `Converted media from the provided link: ${url}`,
+                body: `Downloaded media from the provided link: ${url}`,
                 attachment: fs.createReadStream(filename),
             },
             event.threadID,
             () => fs.unlinkSync(filename) // Delete the file after sending
         );
     } catch (error) {
-        api.sendMessage('An error occurred while converting the media.', event.threadID, event.messageID);
+        api.sendMessage('An error occurred while downloading the media.', event.threadID, event.messageID);
         console.error(error);
     }
 };
+
+// Function to modify Google Drive link to a direct download link
+function modifyToDirectDownloadLink(originalLink) {
+    const fileId = originalLink.match(/\/d\/(.+?)\//)[1];
+    return `https://drive.google.com/uc?export=download&id=${fileId}`;
+}
+
+// Function to get the file extension from a URL
+function getFileExtension(url) {
+    return url.split('.').pop();
+}
