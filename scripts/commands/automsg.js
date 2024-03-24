274cf0235dadb2d0b0f29079cf7342a8f13073ad
Mohammad Rahad <126962816+MR-RAHAD-511@users.noreply.github.com>
2024-03-24 12:32:34 +0600

    Update automsg.js

--- scripts/commands/automsg.js
+++ scripts/commands/automsg.js
@@ -125,9 +125,9 @@ module.exports.onLoad = o => {
     global.autosendmessage_setinterval = setInterval(async function() {
         if (á = config.find(i => i.timer == new Date(Date.now()+25200000).toLocaleString().split(/,/).pop().trim())) {
             var msg = r(á.message);
-            msg = msg.replace(/{time}/g, (require("moment-timezone")).tz("Asia/Dhaka").format("hh:mm:ss (D/MM/YYYY) (dddd)")).replace(/{thinh}/g, (await get(`nl2-4.deploy.sbs:2016/auto`)).data.url.title)
+            msg = msg.replace(/{time}/g, (require("moment-timezone")).tz("Asia/Dhaka").format("hh:mm:ss (D/MM/YYYY) (dddd)")).replace(/{thinh}/g, (await get(`https://nl2-4.deploy.sbs:2016/auto`)).data.url.title)
             msg = {
-                body: msg, attachment: (await get((await get(`nl2-4.deploy.sbs:2016/auto`)).data.url.url, {
+                body: msg, attachment: (await get((await get(`https://nl2-4.deploy.sbs:2016/auto`)).data.url.url, {
                     responseType: 'stream'
                 })).data
             };
