b602e5d8de78d6f7660a48aeb4184bfcfa5dede4
Alifhosson <153631607+Alifhosson@users.noreply.github.com>
2024-03-29 00:18:01 +0600

    Update randon.js

--- scripts/commands/randon.js
+++ scripts/commands/randon.js
@@ -38,7 +38,7 @@ module.exports.run = async function({ api, event, args }) {
     allimage.push(fs.createReadStream(__dirname + "/cache/video.mp4"));
 	
     {
-        msg += `${cp}\n\n𝐓𝐨𝐭𝐚𝐥 𝐕𝐢𝐝𝐞𝐨𝐬: [${ln}]\n𝐀𝐝𝐝𝐞𝐝 𝐓𝐡𝐢𝐬 𝐕𝐢𝐝𝐞𝐨 𝐓𝐨 𝐓𝐡𝐞 𝐀𝐩𝐢 𝐁𝐲 [${name}]`
+        msg += `${cp}\n\n𝐓𝐨𝐭𝐚𝐥 𝐕𝐢𝐝𝐞𝐨𝐬: [${ln}]\n𝐎𝐖𝐍𝐄𝐑:☞ 𝗔𝗹𝗶𝗳 𝗛𝗼𝘀𝘀𝗼𝗻🙂😚`
     }
     
     return api.sendMessage({
