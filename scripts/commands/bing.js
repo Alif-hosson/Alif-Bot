c5338c2e8e131f845bb9eb49b2b189c27f5a42d2
Alifhosson <153631607+Alifhosson@users.noreply.github.com>
2024-04-01 01:42:31 +0600

    Update bing.js

--- scripts/commands/bing.js
+++ scripts/commands/bing.js
@@ -14,7 +14,7 @@ module.exports.run = async function({ api, event, args }) {
     const fs = require("fs-extra");
     const prompt = args.join(" ");
     const key = this.config.credits;
-    if (!prompt) return api.sendMessage('use : /bing cat', event.threadID, event.messageID); 
+    if (!prompt) return api.sendMessage('use : /ping mow', event.threadID, event.messageID); 
 
     const rndm = ['1aMDh8KssNxOR8ig9UxOMc5FTTpihcJ3Dxcpgd4CZwlDH5_zHf2kw4d9Oqf3l4PcRXXtTfnGSihZasvxUHjxACtMitrB61JdX6Su0VpQmk2r-FS1aeEvDuKt7slpeavNxyti3psi6keXS1LoeLG2SCNoYwBaZ3adCM6JnA1NGh0vHU7zSzRepufUr4pzTdYN9SI9Pvh8MCzxZRFADDfavHQ10g__1waaF6O-FSwL5bKOItl9JYRdvwLOmy0X9rD5oRO8eUDjwHDuH1r903oEMS0bGsEcAAPhHRq0oO9-9LwHgG9TFiMpFvqA5y1oIfP8_E9BsHe3lQhkDT_lYiRDxTl5ozogRCthJbpXA_NVxDRLjom02wYkO-8qNLJkz2fAmuHxy0ueAvVzmnEV9Vc6VXu_-f4OFdYpO3mtvkz_h-ZfIA185GgSdC1WSAKzuZzTVJMt9w4DSvIIn5QoY-HnVgPrkLqxQTl6MXH6GRLqITrKi1JJWABzXrQ-3Cz5tkdBGgQDAoJR4FUqTMk6gwL6sR8IwNWWnWq_f3A6qLXvng6DwsjWcMpog7aoDQ-wpPuJoTkTJ8TGxPL408P6ugF3kx2CCjoQn3WIOWEgOQ8bhgDd2FXPXdC0hsdah9iOBeVEUShBg'];  //input cooki here
     var cookie = rndm[Math.floor(Math.random() * rndm.length)];
