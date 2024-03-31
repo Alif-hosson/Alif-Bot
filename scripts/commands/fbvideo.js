7857ca276c4bce862d9e4b931c84ddfae202207e
Alifhosson <153631607+Alifhosson@users.noreply.github.com>
2024-03-31 21:30:39 +0600

    Update fbvideo.js

--- scripts/commands/fbvideo.js
+++ scripts/commands/fbvideo.js
@@ -1,58 +1,42 @@
 module.exports = {
-  config:{
-    name: "fbvideo",
-    version: "0.0.2",
-    permission: 0,
-    prefix: true,
-    credits: "Rahad",
-    description: "fb video",
-    category: "user",
-    usages: "",
-    cooldowns: 5,
-  },
+    },
 
-  languages: {
-    "vi": {},
-    "en": {
-      "missing": '[ ! ] Input link.',
-      "wait": '𝐃𝐎𝐖𝐍𝐋𝐎𝐀𝐃𝐈𝐍𝐆 𝐕𝐈𝐃𝐄𝐎 𝐅𝐎𝐑 𝐘𝐎𝐔\n\n𝐏𝐋𝐄𝐀𝐒𝐄 𝐖𝟖...',
-      "rahad": '✅Downloaded Successfully',
-      "error": '❌Error'
-    }
-  },
-
-  start: async function({ nayan, events, args, lang }) {
-    const axios = require("axios"); // Corrected import
-    const fs = require("fs-extra");
+start: async function({ nayan, events, args, lang }) {
+    const axios = require("axios")
+    const request = require("request")
+    const fs = require("fs-extra")
+  const { ytdown, ndown, tikdown, twitterdown } = require("nayan-media-downloader")
     const { messageID, threadID } = events;
-    if (!args[0]) return nayan.reply(lang("missing"), threadID, messageID);
+  if (!args[0]) return nayan.reply(lang("missing"), threadID, messageID);
+
 
     let np = args.join(" ");
-    if (!args[1]) nayan.reply(lang("wait"), events.threadID, (err, info) => setTimeout(() => { nayan.unsendMessage(info.messageID) }, 20000));
+   if (!args[1]) nayan.reply(lang("wait"), events.threadID, (err, info) => setTimeout(() => { nayan.unsendMessage(info.messageID) }, 20000));
 
-    try {
-      const res = await axios.get(`http://nl2-4.deploy.sbs:2016/fbdl?url=${np}&apikey=xnx`);
-      console.log(res);
-      var msg = [];
-      let img1 = `${res.data.hd}`;
+ try {
+    const res = await ndown(`${np}`);
+console.log(res)
+    var msg = [];
+    let img1 = `${res.data[0].url}`;
 
-      let imgs1 = (await axios.get(`${img1}`, {
+
+    let imgs1 = (await axios.get(`${img1}`, {
         responseType: 'arraybuffer'
-      })).data;
-      fs.writeFileSync(__dirname + "/cache/fbvideo.mp4", Buffer.from(imgs1, "utf-8"));
-      var allimage = [];
-      allimage.push(fs.createReadStream(__dirname + "/cache/fbvideo.mp4"));
+    })).data;
+    fs.writeFileSync(__dirname + "/cache/fbvideo.mp4", Buffer.from(imgs1, "utf-8"));
+    var allimage = [];
+    allimage.push(fs.createReadStream(__dirname + "/cache/fbvideo.mp4"));
 
-      {
-        msg += lang("rahad");
-      }
+    {
+        msg += lang("down")
+    }
 
-      return nayan.reply({
+    return nayan.reply({
         body: msg,
         attachment: allimage
-      }, events.threadID, events.messageID);
-    } catch (err) {
-      nayan.reply(lang("error"), events.threadID, events.messageID);
-    }
-  }
+    }, events.threadID, events.messageID);
+} catch (err) {
+    nayan.reply(lang("error"), events.threadID, events.messageID);  
+   }
+}
 };
