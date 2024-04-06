7e7cd18e5d878f324373661236e4653a3c6ddf8c
Alifhosson <153631607+Alifhosson@users.noreply.github.com>
2024-04-06 14:09:09 +0600

    Update bing.js

--- scripts/commands/bing.js
+++ scripts/commands/bing.js
@@ -1,45 +1,60 @@
-module.exports.config = {
-  name: "bing",
-  version: "1.0.0",
-  hasPermission: 0,
-  credits: "Nayan",
-  description: "",
-  category: "Image",
-  usages: "bing cat",
-  cooldowns: 5
-};
+ module.exports = {
+  config: {
+    name: "bing",
+    version: "1.0.0",
+    permission: 0,
+    credits: "Nayan",
+    description: "",
+    prefix: 'awto',
+    category: "auto prefix",
+    usages: "bing prompt",
+    cooldowns: 10,
+},
 
-module.exports.run = async function({ api, event, args }) {
+   languages: {
+   "vi": {},
+       "en": {
+           "missing": 'use : /bing cat'
+       }
+   },
+
+start: async function({ nayan, events, args, lang}) {
     const axios = require("axios");
     const fs = require("fs-extra");
+    const request = require("request");
     const prompt = args.join(" ");
     const key = this.config.credits;
-    if (!args.length) return api.sendMessage('Usage: /bing [prompt]', event.threadID, event.messageID); 
+    const apis = await axios.get('https://raw.githubusercontent.com/MR-NAYAN-404/NAYAN-BOT/main/api.json')
+  const n = apis.data.api2
+    if(!prompt) return nayan.reply(lang('missing'), events.threadID, events.messageID)
 
-    const rndm = ['SRCHHPGUSR=SRCHLANG=en&CW=360&CH=636&SCW=360&SCH=636&BRW=MM&BRH=MM&DPR=2.0&UTC=360&HV=1711912168&PRVCW=360&PRVCH=636&DM=1'];  //input cooki here
-    var cookie = rndm[Math.floor(Math.random() * rndm.length)];
+  const rndm = ['1VD95z05ScFZXe3oXkS83aEsZOOFP0n96A1seqIJZn3dGtCR18wV3rVe8QO_KlDVotA-EOJeRMg0t56gjbcZnfc4ufo51k1jlPRZ9wdeYYNTgpWP5fyziq7SUGdvbALI0MWQ2EksCbWfWIhoCT3QwW2XrG9n0EOvcy69Sf2X1B-BGiIXLgbqL0ro0DXJNhEUGVRIXRoU08WRKuQbvUFYhMg'] // input your cookie hare
 
-    const res = await axios.get(`https://bing-api-5dpl.onrender.com/bing-img?key=${key}&cookie=${cookie}&prompt=${encodeURIComponent(prompt)}`);
+  var cookie = rndm[Math.floor(Math.random() * rndm.length)];
 
-    console.log(res.data);
+
+    const res = await axios.get(`${n}/bing-img?key=${key}&cookie=${cookie}&prompt=${encodeURIComponent(prompt)}`);
+
+  
+  console.log(res.data)
     const data = res.data.result;
-    const numberSearch = data.length;
+  const numberSearch = data.length
     var num = 0;
     var imgData = [];
     for (var i = 0; i < parseInt(numberSearch); i++) {
-        let path = __dirname + `/cache/${num}.jpg`;
-        let getDown = (await axios.get(`${data[i]}`, { responseType: 'arraybuffer' })).data;
-        fs.writeFileSync(path, Buffer.from(getDown, 'utf-8'));
-        imgData.push(fs.createReadStream(__dirname + `/cache/${num}.jpg`));
-        num += 1;
+      let path = __dirname + `/cache/${num+=1}.jpg`;
+      let getDown = (await axios.get(`${data[i]}`, { responseType: 'arraybuffer' })).data;
+      fs.writeFileSync(path, Buffer.from(getDown, 'utf-8'));
+      imgData.push(fs.createReadStream(__dirname + `/cache/${num}.jpg`));
     }
 
-    await api.sendMessage({
+    
+    nayan.reply({
         attachment: imgData,
-        body: "Bing Search Result\n\nPrompt: " + prompt + "\n\n#Number of Images: " + numberSearch
-    }, event.threadID, event.messageID); 
-
-    for (let ii = 0; ii < parseInt(numberSearch); ii++) {
-        fs.unlinkSync(__dirname + `/cache/${ii}.jpg`);
+        body: "🔍Bing Search Result🔍\n\n📝Prompt: " + prompt + "\n\n#️⃣Number of Images: " + numberSearch
+    }, events.threadID, events.messageID)
+    for (let ii = 1; ii < parseInt(numberSearch); ii++) {
+        fs.unlinkSync(__dirname + `/cache/${ii}.jpg`)
     }
-};
+}
+ }
